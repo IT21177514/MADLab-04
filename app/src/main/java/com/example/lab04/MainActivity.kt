dfa0160f34c966b3a7cca88aba4f2d5beb904c75
IT21177514/MADLab-04 <it21177514@my.sliit.lk>
2023-03-07 12:43:53 +0530

    designed login form

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.lab04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val submitButton=findViewById<Button>(R.id.btnSubmit)
+        var edtNameEditText=findViewById<EditText>(R.id.edtName)
+        var edtEmailEditText=findViewById<EditText>(R.id.edtEmail)
+        var edtPasswordEditText=findViewById<EditText>(R.id.edtPassword)
+        var edtReTypePasswordEditText=findViewById<EditText>(R.id.edtReTypePassword)
+
+
+        lateinit var userName:String
+        lateinit var email:String
+        lateinit var password:String
+        lateinit var reTypePassword:String
+
+        submitButton.setOnClickListener{
+            userName= edtNameEditText.text.toString()
+            email=edtEmailEditText.text.toString()
+            password=edtPasswordEditText.text.toString()
+            reTypePassword=edtReTypePasswordEditText.text.toString()
+
+            if(password==reTypePassword){
+
+            }//end if
+        }//end submitButton.setOnClickListener
     }
 }
